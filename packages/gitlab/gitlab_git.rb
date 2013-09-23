67b1b1fdeca148f136ca84aa6991e492fc566cba
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-09-23 11:05:17 -0500

    Cd into /home/git before configuring git

--- packages/gitlab/gitlab_git.rb
+++ packages/gitlab/gitlab_git.rb
@@ -1,5 +1,5 @@
 package :gitlab_git do
-  runner "sudo -u git -H git config --global user.name \"GitLab\""
-  runner "sudo -u git -H git config --global user.email \"gitlab@localhost\""
-  runner "sudo -u git -H git config --global core.autocrlf input"
+  runner "cd /home/git && sudo -u git -H git config --global user.name \"GitLab\""
+  runner "cd /home/git && sudo -u git -H git config --global user.email \"gitlab@localhost\""
+  runner "cd /home/git && sudo -u git -H git config --global core.autocrlf input"
 end
