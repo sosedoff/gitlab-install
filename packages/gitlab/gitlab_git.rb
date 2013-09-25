73ac12509f5a10b2c2f9354aa340e956b84f4893
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-09-24 22:24:08 -0500

    Hook up gitlab_git to config

--- packages/gitlab/gitlab_git.rb
+++ packages/gitlab/gitlab_git.rb
@@ -1,13 +1,16 @@
 package :gitlab_git do
   path = "/home/git"
 
-  runner "cd #{path} && sudo -u git -H git config --global user.name \"GitLab\""
-  runner "cd #{path} && sudo -u git -H git config --global user.email \"gitlab@localhost\""
+  user_name  = $config.git.user_name
+  user_email = $config.git.user_email
+
+  runner "cd #{path} && sudo -u git -H git config --global user.name \"#{user_name}\""
+  runner "cd #{path} && sudo -u git -H git config --global user.email \"#{user_email}\""
   runner "cd #{path} && sudo -u git -H git config --global core.autocrlf input"
 
   verify do
-    runs_without_error "cd #{path} && sudo -u git -H git config --get user.name | grep GitLab"
-    runs_without_error "cd #{path} && sudo -u git -H git config --get user.email | grep gitlab@localhost"
+    runs_without_error "cd #{path} && sudo -u git -H git config --get user.name | grep #{user_name}"
+    runs_without_error "cd #{path} && sudo -u git -H git config --get user.email | grep #{user_email}"
     runs_without_error "cd #{path} && sudo -u git -H git config --get core.autocrlf | grep input"
   end
 end
