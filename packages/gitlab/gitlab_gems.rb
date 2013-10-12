7bde83c32d96d57769a5d2720c3f1d2719833a00
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-10-12 18:39:04 -0500

    Do not verify bundle installation, it'll be automatically cached

--- packages/gitlab/gitlab_gems.rb
+++ packages/gitlab/gitlab_gems.rb
@@ -3,9 +3,4 @@ package :gitlab_gems do
 
   path = "/home/git/gitlab"
   runner "cd #{path} && sudo -u git -H bundle install --deployment --without development test mysql aws"
-
-  verify do
-    has_gem "charlock_holmes", "0.6.9.4"
-    has_directory "#{path}/vendor/bundle"
-  end
 end
